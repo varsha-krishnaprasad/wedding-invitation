c79fd512957522935a0b179ab8cae465b7358c72
chiragasourabh <chiragasourabh@gmail.com>
2021-02-06 16:32:12 +0530

    changes

--- static/js/heart.js
+++ static/js/heart.js
@@ -1,4 +1,4 @@
-var love = setInterval(function() {
+var love = setInterval(function () {
     var r_num = Math.floor(Math.random() * 40) + 1;
     var r_size = Math.floor(Math.random() * 65) + 10;
     var r_left = Math.floor(Math.random() * 100) + 1;
@@ -9,7 +9,7 @@ var love = setInterval(function() {
 
     $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
 
-    $('.heart').each(function() {
+    $('.heart').each(function () {
         var top = $(this).css("top").replace(/[^-\d\.]/g, '');
         var width = $(this).css("width").replace(/[^-\d\.]/g, '');
         if (top <= -100 || width >= 150) {
